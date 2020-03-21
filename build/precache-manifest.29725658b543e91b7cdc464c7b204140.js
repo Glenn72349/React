2723f10d83775eacecbe6342f48993ce834eeae8
Glenn72349 <62481485+Glenn72349@users.noreply.github.com>
2020-03-21 23:33:21 +0300

    Add files via upload

---
+++ build/precache-manifest.29725658b543e91b7cdc464c7b204140.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc8014cd9a8a0ff4fdbe3b37756ee41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12a6c9bd5cd1c42d13ca",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4329c1001104fcc5ff9a",
+    "url": "/static/js/2.bcfebd7b.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.bcfebd7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a6c9bd5cd1c42d13ca",
+    "url": "/static/js/main.6b71b9fa.chunk.js"
+  },
+  {
+    "revision": "d3becf983bbae4d9ba91",
+    "url": "/static/js/runtime-main.e39b4edb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
